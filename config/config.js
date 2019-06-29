bb89245750441bfb040cdf2f1d465b3e71f7db1d
ajaysingh0595 <ajay.singh0595@gmail.com>
2019-06-29 17:12:19 +0530

    config change

--- config/config.js
+++ config/config.js
@@ -1,7 +1,5 @@
 const config = {
     PORT: 3000,
-    mongoDB: 'mongodb://shorturldb:shorturldb123@ec2-52-15-75-251.us-east-2.compute.amazonaws.com/shorturldb'
+    mongoDB: 'mongodb://localhost:27017/urlshort'
 }
 module.exports = config;
-// 'mongodb://localhost:27017/urlshort'
-// mongodb://user:root4861@ec2-52-15-75-251.us-east-2.compute.amazonaws.com/eventapp
